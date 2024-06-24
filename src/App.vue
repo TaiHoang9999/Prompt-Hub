e259c6e1824314da4aff9102f86ebc481f628649
TaiHoang9999 <contact.taihoang@gmail.com>
2024-06-24 15:11:19 +0700

    Init

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import OneTimeCredit from "./components/OneTimeCredit.vue"
+</script>
+
+<template>
+  <OneTimeCredit />
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,42 @@
+<script setup></script>
+
+<template>
+        <header class="bg-white">
+                <nav class="flex justify-between items-center bg-[#F9FAFB]">
+                        <div>
+                                <img class="" src="../assets/img/Logo.svg" alt="OnlyPromptsLogo" />
+                        </div>
+
+                        <div class="">
+                                <ul
+                                        class="flex items-center gap-[4vw] text-[16px] text-[#161C24] font-[600] leading-[24px]"
+                                >
+                                        <li>
+                                                <a href="">Why us</a>
+                                        </li>
+                                        <li>
+                                                <a href="">Our product</a>
+                                        </li>
+                                        <li>
+                                                <a href="">Blog</a>
+                                        </li>
+                                </ul>
+                        </div>
+
+                        <div class="flex justify-between items-center">
+                                <button
+                                        class="text-[13px] leading-5 px-[4px] py-[8px] border-[1px] border-solid border-[#637381] rounded-[4px]"
+                                >
+                                        Explore new prompts
+                                </button>
+                                <button
+                                        class="bg-[#CDE200] text-[#161C24] text-[13px] leading-5 px-[4px] py-[8px] rounded-[4px]"
+                                >
+                                        Access my prompts
+                                </button>
+                        </div>
+                </nav>
+        </header>
+</template>
+
+<style scoped></style>
